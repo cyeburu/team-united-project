6f49b49e7f1551bfe4ed2b55758ca7cd1dfd81fb
cy <cyeburu@yahoo.com>
2020-08-13 21:30:37 +0100

    sorted glossary list

--- team-united/src/GlossaryList.js
+++ team-united/src/GlossaryList.js
@@ -1,14 +1,15 @@
 import React from "react";
 import TermsData from "./TermsData.json";
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const GlossaryList = () => {
+  let sortData = TermsData.sort((a, b) => a.name.localeCompare(b.name));
   return (
     <div>
       <h1 className="title">Cyf Glossary</h1>
       <ul className="container  list-unstyled list-group list-group-striped  col-4">
-        {TermsData.map((terms, index) => {
+        {sortData.map((terms, index) => {
           return (
             <li className="col-12" key={index}>
               <Link to={`/TermsDescription/${index}`}>{terms.name}</Link>
