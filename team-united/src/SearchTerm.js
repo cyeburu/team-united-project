423e6f67e482e2bc64b9b4552e4eefe96a3d4b38
[Alisina92] <asb92.jobs@gmail.com>
2020-08-15 20:06:53 +0100

    Team-united-week1-Final

--- team-united/src/SearchTerm.js
+++ team-united/src/SearchTerm.js
@@ -1,27 +1,20 @@
-import React, { useState } from "react";
-
-
+import React from "react";
 
 const SearchTerm = ({ search, setSearch }) => {
-    
-    function handleChange(event)
-    {
-        setSearch(event.target.value);
-    }
+  const handleChange=(event)=> {
+    setSearch(event.target.value);
+  }
 
-
-return (
+  return (
     <div>
-     
-          <input 
-          className="border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-lg text-sm focus:outline-none" 
-          type="text"
-          onChange={handleChange} 
-          value={search}
-          placeholder="search for a term"
-          />
-       
+      <input
+        className="border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-lg text-sm focus:outline-none"
+        type="text"
+        onChange={handleChange}
+        value={search}
+        placeholder="search for a term"
+      />
     </div>
-    );
-}
+  );
+};
 export default SearchTerm;
